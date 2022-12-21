f54f4a16561176af53a656181460fcce0e344b65
Khietn <37112074+Khietn@users.noreply.github.com>
2022-12-21 22:52:49 +0700

    Add files via upload

---
+++ src/main/java/com/discord/App.java
@@ -0,0 +1,36 @@
+package com.discord;
+
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+import discord4j.core.object.entity.channel.MessageChannel;
+import reactor.core.publisher.Mono;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	final static String token = "+++++ODg4MDcwNjUyODU3Mjg2NjY3.G-vM8g.6sTxozsojCyADQOVc380--5LlhUdHlcDY8BStI";
+
+	public static void main(String[] args) {
+		DiscordClient client = DiscordClient.create(token.replace("+", ""));
+		Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
+	    gateway.on(MessageCreateEvent.class, event -> {
+	      Message message = event.getMessage();
+	      System.out.println("Test: " + 	      event.getMessage().getData().content());
+	      if (message.getContent().equalsIgnoreCase("lz Long")) {
+	          return message.getChannel()
+	              .flatMap(channel -> channel.createMessage("Long ngu nhu con cho djt me m Long"));
+	      }
+
+	      return Mono.empty();
+	    }));
+		login.block();
+		
+	}
+
+}
